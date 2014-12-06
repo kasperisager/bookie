556e07965d60b92425009eab499ddb219193f0f8
Kasper Isager <kasperisager@gmail.com>
2014-12-06 11:46:42 +0100

    Handle data querying in Application controller.

--- src/main/java/dk/itu/bookie/controller/ApplicationController.java
+++ src/main/java/dk/itu/bookie/controller/ApplicationController.java
@@ -9,22 +9,69 @@ import java.util.ResourceBundle;
 // Net utilities
 import java.net.URL;
 
+// SQL utilities
+import java.sql.SQLException;
+
+// JavaFX collections
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 // FXML utilities
-import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+
+// Donkey utilities
+import dk.itu.donkey.Model;
+
+// Models
+import dk.itu.bookie.model.Showtime;
+import dk.itu.bookie.model.Reservation;
 
 /**
  * Application controller class.
  *
  * @version 1.0.0
  */
-public final class ApplicationController implements Initializable {
+public final class ApplicationController {
   private static ApplicationController instance;
+
+  private static ObservableList<Showtime> showtimes;
+
+  private static ObservableList<Reservation> reservations;
 
   public ApplicationController getInstance() {
     return ApplicationController.instance;
   }
 
-  public void initialize(URL url, ResourceBundle resourceBundle) {
+  public void initialize() {
     ApplicationController.instance = this;
+  }
+
+  public static ObservableList<Showtime> showtimes() throws SQLException {
+    if (ApplicationController.showtimes == null) {
+      ApplicationController.showtimes = FXCollections.observableArrayList(
+        Model
+          .find(Showtime.class)
+          .where("playingat", ">", System.currentTimeMillis())
+          .orderBy("movies.name")
+          .orderBy("playingat")
+          .get()
+      );
+    }
+
+    return ApplicationController.showtimes;
+  }
+
+  public static ObservableList<Reservation> reservations() throws SQLException {
+    if (ApplicationController.reservations == null) {
+      ApplicationController.reservations = FXCollections.observableArrayList(
+        Model
+          .find(Reservation.class)
+          .where("showtimes.playingat", ">", System.currentTimeMillis())
+          .orderBy("showtimes.playingat")
+          .get()
+      );
+    }
+
+    return ApplicationController.reservations;
   }
 }
